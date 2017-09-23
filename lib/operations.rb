9447cf480d21dc55d6efaaeecb6944931bf89128
Matt Hough <matt@hough.tv>
2017-09-23 23:55:20 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,9 +1,11 @@
-loop do
-	counter += 1
+def methods
+	loop do
+		counter += 1
 
-	puts "Trip number #{counter} around the earth!"
+		puts "Trip number #{counter} around the earth!"
 
-	if counter >= 10
-		break
+		if counter >= 10
+			break
+		end
 	end
 end

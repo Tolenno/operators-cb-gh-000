9123cc4ce2b4124e0509a501e5e2ec697a468afb
Matt Hough <matt@hough.tv>
2017-09-23 23:53:53 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -7,3 +7,14 @@ end
 def not_safe?(speed)
 	!speed.between?(40,60) ? true : false
 end
+
+
+loop do
+	counter += 1
+
+	puts "Trip number #{counter} around the earth!"
+
+	if counter >= 10
+		break
+	end
+end
